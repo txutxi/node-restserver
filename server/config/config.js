eef8e0eb70cdcdc55cf6c681ab40d9960060525d
txutxi <txutxi@casa.com>
2020-04-12 19:23:53 +0100

    FIX: Variable de entorno config en heroku

--- server/config/config.js
+++ server/config/config.js
@@ -19,7 +19,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'MONGO_URI';
+    urlDB = MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
